3b6d5fce2b7e9fd68701ca5c4ed1a2a631ba19a1
Abhijith <abhijithknambiar@outlook.com>
2024-01-20 11:22:29 +0530

    nothing new

---
+++ food/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+# Create your views here.
+
+
+def index(request):
+    item_list= Item.objects.all()
+    context={
+        'item_list': item_list,
+        }
+    return render(request,'food\index.html',context)
+
+def item(request):
+    return HttpResponse('This is an item view')
+
+def detail(request,item_id):
+    return HttpResponse("This is item no %s" %item_id)
+    pass
